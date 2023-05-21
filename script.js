6252a3938ec4a4fbe13a94141df161b4b36eb4bb
Shweta pal <51901918+Shwetapal9@users.noreply.github.com>
2023-05-22 00:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-const sizeInfo = document.getElementById("h1");
+const screensize = document.getElementById("sizeInfo");
 const expectedWidth =window.innerWidth;
 const expectedHeight=  window.innerHeight;
-sizeInfo.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
+screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
 document.body.append(sizeInfo);
 
 window.onresize= function(){
 	
-	sizeInfo.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
+	screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
 	// document.body.append(sizeElement);
 }
