a9c2a6ad30e95d20fc310b4390570561b381b294
Shweta pal <51901918+Shwetapal9@users.noreply.github.com>
2023-05-22 00:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const screensize = document.createElement("h1");
 screensize.id= "sizeInfo";
-const expectedWidth =window.innerWidth;
+const  expectedWidth =window.innerWidth;
 const expectedHeight=  window.innerHeight;
 screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
 document.body.append(screensize);
