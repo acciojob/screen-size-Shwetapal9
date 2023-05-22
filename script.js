23dfee1ea7d536609e017ed88ed3e7bba8786acb
Shweta pal <51901918+Shwetapal9@users.noreply.github.com>
2023-05-22 14:33:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 //your JS code here. If required.
-const node = document.getElementsByTagName("div")[0];
-const screensize = document.createElement("h1");
-screensize.id= "sizeInfo";
-const  expectedWidth =window.innerWidth;
-const expectedHeight=  window.innerHeight;
-screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
-document.node.appendChild(screensize);
+// const node = document.getElementsByTagName("div")[0];
+const screensize = document.getElementById("sizeInfo");
+const width = sizeInfo.querySelector("h1:first-child");
+const height = sizeInfo.querySelector("h1:last-child");
+// screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
+// document.node.appendChild(screensize);
 
-window.onresize= function(){
-	
-	screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
-	// document.body.append(sizeElement);
+function setScreenSize() {
+width.innerText = `Width: ${window.innerWidth}px`;
+height.innerText = `Height: ${window.innerHeight}px`;
 }
+window.addEventListener('resize', setScreenSize);
+setScreenSize();
+
+// window.onresize= function(){
+	
+// 	screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
+// 	// document.body.append(sizeElement);
+// }
